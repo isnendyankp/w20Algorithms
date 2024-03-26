76d47a726acbd81e8b2e891744c932ee4421d664
isnendyankp <isnendyankp@gmail.com>
2024-03-26 16:49:57 +0700

    Cr8 Big O notation with example 1 contanst time

---
+++ BigOnotation.ts
@@ -0,0 +1,17 @@
+// Big O notation
+
+// O(1) - Constant Time
+// example Constant Time
+
+// this code will always run in the same amount of time regardless of the size of the array
+function log(array: number[]) {
+  console.log(array[0]);
+  console.log(array[1]);
+}
+
+// O(log n) - Logarithmic Time
+// O(n) - Linear Time
+// O(n log n) - Linearithmic Time
+// O(n^2) - Quadratic Time
+// O(2^n) - Exponential Time
+// O(n!) - Factorial Time
