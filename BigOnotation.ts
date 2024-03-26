cc0e728bff61e01055497e06aea19244c6c06c3c
isnendyankp <isnendyankp@gmail.com>
2024-03-26 16:54:41 +0700

    cr8 linear time example no 1

--- BigOnotation.ts
+++ BigOnotation.ts
@@ -1,7 +1,6 @@
 // Big O notation
 
 // O(1) - Constant Time
-// example Constant Time
 // this code will always run in the same amount of time regardless of the size of the array
 // because it only logs the first element of the array
 // No 1.
@@ -14,8 +13,14 @@ function log(array: number[]) {
 def contant_time_example(arr):
     return Array[0]
 
-// O(log n) - Logarithmic Time
 // O(n) - Linear Time
+// this code will run in linear time because it will log each element of the array
+// No 1.
+function logAll(array: number[]) {
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+  }
+}
 // O(n log n) - Linearithmic Time
 // O(n^2) - Quadratic Time
 // O(2^n) - Exponential Time
