9b0cf741113090a4b9699a875fbcf97e129d7f08
isnendyankp <isnendyankp@gmail.com>
2024-03-26 16:53:17 +0700

    cr8 example 2 constant time

--- BigOnotation.ts
+++ BigOnotation.ts
@@ -2,12 +2,17 @@
 
 // O(1) - Constant Time
 // example Constant Time
-
 // this code will always run in the same amount of time regardless of the size of the array
+// because it only logs the first element of the array
+// No 1.
 function log(array: number[]) {
   console.log(array[0]);
   console.log(array[1]);
 }
+
+// No 2.
+def contant_time_example(arr):
+    return Array[0]
 
 // O(log n) - Logarithmic Time
 // O(n) - Linear Time
