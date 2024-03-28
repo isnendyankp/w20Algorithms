dbc895162365d68e8f2143e967fefc537c42cbb3
isnendyankp <isnendyankp@gmail.com>
2024-03-28 19:12:27 +0700

    Add linearithmic time example no.2 sorting algorithm

--- BigOnotation.ts
+++ BigOnotation.ts
@@ -116,3 +116,24 @@ function merge(left: number[], right: number[]) {
 
   return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 }
+// No 2.
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+    return merge(merge_sort(left), merge_sort(right))
+
+def merge(left, right):
+    result = []
+    left_index = 0
+    right_index = 0
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            result.append(left[left_index])
+            left_index += 1
+        else:
+            result.append(right[right_index])
+            right_index += 1
+    return result + left[left_index:] + right[right_index:]
