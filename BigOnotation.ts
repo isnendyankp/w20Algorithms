a27ddae123262b005afcb33392984de7d3dda53a
isnendyankp <isnendyankp@gmail.com>
2024-03-28 19:11:33 +0700

    Refactor merge function in BigOnotation.ts

--- BigOnotation.ts
+++ BigOnotation.ts
@@ -98,3 +98,21 @@ function sort(array: number[]) {
 
   return merge(sort(left), sort(right));
 }
+
+function merge(left: number[], right: number[]) {
+  let result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
