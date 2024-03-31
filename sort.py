0d14c3e91032e4571e025c0409b6192faad36b0c
isnendyankp <isnendyankp@gmail.com>
2024-03-31 15:13:00 +0700

    Add heap sort algorithm to sort.py

--- sort.py
+++ sort.py
@@ -111,3 +111,34 @@ print("Sorted array is:", arr)
 
 # Output
 # Sorted array is: [11, 12, 22, 25, 34, 64, 90]
+
+# heap sort
+# this algorithm is efficient for large lists and is recommended for use in production code.
+def heapify(arr, n, i):
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+    if l < n and arr[i] < arr[l]:
+        largest = l
+    if r < n and arr[largest] < arr[r]:
+        largest = r
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+def heap_sort(arr):
+    n = len(arr)
+    for i in range(n//2 - 1, -1, -1):
+        heapify(arr, n, i)
+    for i in range(n-1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+    return arr
+
+# Test
+arr = [64, 34, 25, 12, 22, 11, 90]
+heap_sort(arr)
+print("Sorted array is:", arr)
+
+# Output
+# Sorted array is: [11, 12, 22, 25, 34, 64, 90]
