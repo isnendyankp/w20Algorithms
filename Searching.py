e89d23edf430de535c01e6835ed50f11a5e75340
isnendyankp <isnendyankp@gmail.com>
2024-04-03 18:56:32 +0700

    Add hash table implementation for efficient searching

--- Searching.py
+++ Searching.py
@@ -126,3 +126,37 @@ else:
 
 # Output
 # Element found at index 4
+
+# Hash Table with hash map
+# This algorithm for searching an element in a list is efficient for large lists and is recommended for use in production code.
+
+class HashTable:
+    def __init__(self):
+        self.MAX = 100
+        self.arr = [None for i in range(self.MAX)]
+
+    def get_hash(self, key):
+        h = 0
+        for char in key:
+            h += ord(char)
+        return h % self.MAX
+
+    def __setitem__(self, key, val):
+        h = self.get_hash(key)
+        self.arr[h] = val
+
+    def __getitem__(self, key):
+        h = self.get_hash(key)
+        return self.arr[h]
+
+# Test
+t = HashTable()
+t["march 6"] = 310
+t["march 7"] = 420
+t["march 8"] = 67
+t["march 17"] = 63457
+print(t["march 6"])
+
+# Output
+# 310
+
