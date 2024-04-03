7bd2b7b31409e43105b3e4d50bc1e41aabb9d95e
isnendyankp <isnendyankp@gmail.com>
2024-04-03 18:53:03 +0700

    Add binary search algorithm to Searching.py

--- Searching.py
+++ Searching.py
@@ -51,3 +51,38 @@ else:
 
 # Output
 # Element found at index 4
+    
+# Binary Search
+# This algorithm for searching an element in a list is efficient for large lists and is recommended for use in production code.
+    
+def binary_search(arr, x):
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+
+    while low <= high:
+        mid = (high + low) // 2
+
+        # If x is greater, ignore left half
+        if arr[mid] < x:
+            low = mid + 1
+
+        # If x is smaller, ignore right half
+        elif arr[mid] > x:
+            high = mid - 1
+
+        # x is present at mid
+        else:
+            return mid
+
+    # If we reach here, then the element was not present
+    return -1
+
+# Test
+arr = [2, 3, 4, 10, 40]
+x = 10
+result = binary_search(arr, x)
+if result != -1:
+    print(f"Element found at index {result}")
+else:
+    print("Element not found")
