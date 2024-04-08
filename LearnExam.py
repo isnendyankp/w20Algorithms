da78887ecb72b7f257641e4065b74e44619b35b7
isnendyankp <isnendyankp@gmail.com>
2024-04-08 22:49:44 +0700

    Refactor merge function in Mergesort algorithm

--- LearnExam.py
+++ LearnExam.py
@@ -109,7 +109,22 @@ def Mergesort(arr):
 
     return merge(left, right)
 
-    return arr
+def merge(left, right):
+    result = []
+    i = 0
+    j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
 
 # keep this function call here
 # print Mergesort(raw_input())
