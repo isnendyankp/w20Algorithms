76544d4df4b84869ff51eb888569deb18d4bc4a2
isnendyankp <isnendyankp@gmail.com>
2024-04-12 08:47:39 +0700

    Add HashTable class with hash map and collision handling to Searching.py

--- Searching.py
+++ Searching.py
@@ -160,3 +160,34 @@ print(t["march 6"])
 # Output
 # 310
 
+# Hash Table with hash map and collision handling
+# This algorithm for searching an element in a list is efficient for large lists and is recommended for use in production code.
+
+class HashTable:
+    def __init__(self):
+        self.MAX = 10
+        self.arr = [[] for i in range(self.MAX)]
+
+    def get_hash(self, key):
+        h = 0
+        for char in key:
+            h += ord(char)
+        return h % self.MAX
+
+    def __setitem__(self, key, val):
+        h = self.get_hash(key)
+        found = False
+        for idx, element in enumerate(self.arr[h]):
+            if len(element) == 2 and element[0] == key:
+                self.arr[h][idx] = (key, val)
+                found = True
+                break
+        if not found:
+            self.arr[h].append((key, val))
+
+    def __getitem__(self, key):
+        h = self.get_hash(key)
+        for element in self.arr[h]:
+            if element[0] == key:
+                return element[1]
+
